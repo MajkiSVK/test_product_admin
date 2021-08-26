130e1fc142d60812fe97c24daa62f66f82402ef0
MajkiSVK <44mikulas.tomas@gmail.com>
2021-08-26 16:44:59 +0200

    added created_at and updated_at values

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -15,12 +15,12 @@ class ProductsSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-            ['name' => 'Green shirt', 'description' =>'this is green shirt', 'unique_hash' =>'8b52b5ae603e6d99'],
-            ['name' => 'Red shirt', 'description' =>'this is red shirt', 'unique_hash' =>'42835f5cb13a35e4'],
-            ['name' => 'Small hat', 'description' =>'this is small hat', 'unique_hash' =>'a2f8d8b760194584'],
-            ['name' => 'Big hat', 'description' =>'this is big hat', 'unique_hash' =>'e6ff9a29030e0de3'],
-            ['name' => 'Leather handbag', 'description' =>'this is leather handbag', 'unique_hash' =>'e1acd347331b4eee'],
-            ['name' => 'Cloth handbag', 'description' =>'this is cloth handbag', 'unique_hash' =>'e1acd347331b4eee'],
+            ['name' => 'Green shirt', 'description' =>'this is green shirt', 'unique_hash' =>'8b52b5ae603e6d99','created_at' =>now(), 'updated_at' =>now()],
+            ['name' => 'Red shirt', 'description' =>'this is red shirt', 'unique_hash' =>'42835f5cb13a35e4','created_at' =>now(), 'updated_at' =>now()],
+            ['name' => 'Small hat', 'description' =>'this is small hat', 'unique_hash' =>'a2f8d8b760194584','created_at' =>now(), 'updated_at' =>now()],
+            ['name' => 'Big hat', 'description' =>'this is big hat', 'unique_hash' =>'e6ff9a29030e0de3','created_at' =>now(), 'updated_at' =>now()],
+            ['name' => 'Leather handbag', 'description' =>'this is leather handbag', 'unique_hash' =>'e1acd347331b4eee','created_at' =>now(), 'updated_at' =>now()],
+            ['name' => 'Cloth handbag', 'description' =>'this is cloth handbag', 'unique_hash' =>'e1acd347331b4eee','created_at' =>now(), 'updated_at' =>now()],
         ]);
     }
 }
